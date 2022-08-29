88745708baa82cd8357b9f7ddd4c04c05c39382f
Erdi Peker <erdipeker@gmail.com>
2022-08-29 14:24:05 +0300

    Javascript-1 Homework

---
+++ Homeworks/Javascript_Course/Javascript_1-Saat_ve_karşılama/js/clock.js
@@ -0,0 +1,23 @@
+let givenName = prompt("Lütfen isminizi belitiniz : ");
+
+const nameDOM = document.querySelector("#myName");
+nameDOM.innerHTML = `${givenName}`;
+
+function showTime() {
+  let date = new Date();
+
+  let hours = correctTime(date.getHours());
+  let mins = correctTime(date.getMinutes());
+  let secs = correctTime(date.getSeconds());
+
+  let clockDOM = document.querySelector("#myClock");
+  clockDOM.innerHTML = `${hours}:${mins}:${secs}`;
+  setTimeout(showTime, 1000);
+}
+function correctTime(t) {
+  if (t < 10) {
+    return "0" + t;
+  }
+  return t;
+}
+showTime();
