4fcc4b864b5ed2d54e1e00839b69036e7aea2d3a
Erdi Peker <erdipeker@gmail.com>
2022-10-16 13:52:10 +0300

    added BMI calculator project

---
+++ Homeworks/Java101/src/BMICalculator/Main.java
@@ -0,0 +1,17 @@
+package BMICalculator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double height,weight,bmi;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz:");
+        height=input.nextDouble();
+        System.out.print("Lütfen kilonuzu (kg cinsinden) giriniz:");
+        weight=input.nextDouble();
+
+        bmi=weight/(height*height);
+        System.out.println("Vücut Kitle İndeksiniz:" + bmi);
+    }
+}
